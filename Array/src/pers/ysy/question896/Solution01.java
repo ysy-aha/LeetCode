aa6ed0ce0253dee53e229bbb49c5b0d0ab05cf65
yshy3 <yshy360122@qq.com>
2020-10-12 10:12:02 +0800

    896. 单调数列

---
+++ Array/src/pers/ysy/question896/Solution01.java
@@ -0,0 +1,32 @@
+package pers.ysy.question896;
+
+/**
+ * 设置标记 flag，标记为0是未定，标记为1是递增，标记为2是递减
+ * 循环，根据前面的值设置标记
+ *
+ * 执行用时：2 ms, 在所有 Java 提交中击败了46.10%的用户
+ * 内存消耗：46.4 MB, 在所有 Java 提交中击败了82.46%的用户
+ * @Author ysy
+ * @Date 2020/10/12
+ **/
+public class Solution01 {
+    public boolean isMonotonic(int[] A) {
+        int flag = 0;
+        for (int i = 1; i < A.length; i++) {
+            if (flag == 0) {
+                if (A[i] > A[i - 1]) {
+                    flag = 1;
+                } else if (A[i] < A[i - 1]) {
+                    flag = 2;
+                }
+            } else {
+                if (flag == 1 && A[i] < A[i - 1]) {
+                    return false;
+                } else if (flag == 2 && A[i] > A[i - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
