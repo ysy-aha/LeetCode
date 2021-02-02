bd58f6899120b9da808e9f2d75a22274710ec799
yshy3 <yshy360122@qq.com>
2021-02-02 09:46:23 +0800

    237. 删除链表中的节点

---
+++ Section200/src/pers/yshy/question237/ListNode.java
@@ -0,0 +1,16 @@
+package pers.yshy.question237;
+
+/**
+ * 链表对象
+ * @author ysy
+ * @date 2021/2/2
+ * @package pers.yshy.question237
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
