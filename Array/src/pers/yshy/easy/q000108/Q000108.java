e665c55a59f629d98bcbb75baf266bb1bbae2fb4
ysy-aha <yshy360122@qq.com>
2023-03-14 23:21:52 +0800

    108. 将有序数组转换为二叉搜索树

--- Array/src/pers/yshy/easy/q000108/Q000108.java
+++ Array/src/pers/yshy/easy/q000108/Q000108.java
@@ -1,6 +1,10 @@
 package pers.yshy.easy.q000108;
 
+import java.util.LinkedList;
+
 /**
+ * 108. 将有序数组转换为二叉搜索树
+ *
  * @author ysy
  * @since 2022/11/11
  */
@@ -12,31 +16,44 @@ public class Q000108 {
 
         nums = new int[]{-10, -3, 0, 5, 9};
         treeNode = sortedArrayToBST(nums);
-        System.out.println("-------");
+        levelTraverse(treeNode);
+        System.out.println("\n-------");
         nums = new int[]{1, 3};
         treeNode = sortedArrayToBST(nums);
-        System.out.println("-------");
+        levelTraverse(treeNode);
+        System.out.println("\n-------");
     }
 
-    /**
-     * 时间：0 ms，击败：100%
-     * 内存：41.2 MB，击败：74.10%
-     *
-     * @param nums
-     * @return
-     */
-    private static TreeNode sortedArrayToBST(int[] nums) {
-        return buildTree(nums, 0, nums.length);
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        return buildTree(nums, 0, nums.length - 1);
     }
 
     private static TreeNode buildTree(int[] nums, int left, int right) {
-        if (left >= right) {
+        if(left > right) {
             return null;
         }
-        int mid = (left + right) / 2;
-        TreeNode tree = new TreeNode(nums[mid]);
-        tree.left = buildTree(nums, left, mid);
-        tree.right = buildTree(nums, mid + 1, right);
-        return tree;
+        int middle = (right + left) / 2;
+        TreeNode treeNode = new TreeNode(nums[middle]);
+        treeNode.left = buildTree(nums, left, middle-1);
+        treeNode.right = buildTree(nums, middle + 1, right);
+        return treeNode;
+    }
+
+    public static void levelTraverse(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            System.out.print(node.val + "  ");
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
     }
 }
